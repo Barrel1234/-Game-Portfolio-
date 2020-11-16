80e26f10ab663c9ae9501a0f91ec5ad5df58d455
Supreme <supremedatta@gmail.com>
2020-11-16 12:35:35 +0530

    Initial Commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.supreme = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.h1 = new System.Windows.Forms.PictureBox();
+            this.h2 = new System.Windows.Forms.PictureBox();
+            this.h3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.supreme)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(521, 23);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(552, 11);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(135, 24);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(710, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // supreme
+            // 
+            this.supreme.Image = ((System.Drawing.Image)(resources.GetObject("supreme.Image")));
+            this.supreme.Location = new System.Drawing.Point(12, 154);
+            this.supreme.Name = "supreme";
+            this.supreme.Size = new System.Drawing.Size(113, 112);
+            this.supreme.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.supreme.TabIndex = 3;
+            this.supreme.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // h1
+            // 
+            this.h1.BackColor = System.Drawing.Color.Red;
+            this.h1.Location = new System.Drawing.Point(380, 264);
+            this.h1.Name = "h1";
+            this.h1.Size = new System.Drawing.Size(35, 261);
+            this.h1.TabIndex = 4;
+            this.h1.TabStop = false;
+            // 
+            // h2
+            // 
+            this.h2.BackColor = System.Drawing.Color.Red;
+            this.h2.Location = new System.Drawing.Point(584, 359);
+            this.h2.Name = "h2";
+            this.h2.Size = new System.Drawing.Size(35, 166);
+            this.h2.TabIndex = 5;
+            this.h2.TabStop = false;
+            // 
+            // h3
+            // 
+            this.h3.BackColor = System.Drawing.Color.Red;
+            this.h3.Location = new System.Drawing.Point(785, 191);
+            this.h3.Name = "h3";
+            this.h3.Size = new System.Drawing.Size(35, 334);
+            this.h3.TabIndex = 6;
+            this.h3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Yellow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(291, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(450, 91);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Game Over";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(1081, 528);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.h3);
+            this.Controls.Add(this.h2);
+            this.Controls.Add(this.h1);
+            this.Controls.Add(this.supreme);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Name = "Form1";
+            this.Text = " Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.supreme)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.h3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox supreme;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox h1;
+        private System.Windows.Forms.PictureBox h2;
+        private System.Windows.Forms.PictureBox h3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,104 @@
+﻿using NAudio.CoreAudioApi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MMDeviceEnumerator en = new MMDeviceEnumerator();
+            var devices = en.EnumerateAudioEndPoints(DataFlow.All, DeviceState.Active);
+            comboBox1.Items.AddRange(devices.ToArray());
+            label1.Visible = false;
+
+        }
+        public int voice;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            label1.Visible = false;
+            h1.Left = 1000;
+            h2.Left = 1100;
+            h3.Left = 1200;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedItem != null)
+            {
+                var selected = (MMDevice)comboBox1.SelectedItem;
+                voice = (int)(selected.AudioMeterInformation.MasterPeakValue * 100 + 0.5);
+                progressBar1.Value = voice;
+            }
+        }
+        void movehurdle()
+        {
+            if(h1.Left>0)
+            {
+                h1.Left += -2;
+            }
+            else
+            {
+                h1.Left = 1000;
+            }
+            if (h2.Left > 0)
+            {
+                h2.Left += -2;
+            }
+            else
+            {
+                h2.Left = 1050;
+            }
+            if (h3.Left > 0)
+            {
+                h3.Left += -2;
+            }
+            else
+            {
+                h3.Left = 1075;
+            } 
+        }
+         void gameOver()
+        {
+          if((h1.Bounds.IntersectsWith(supreme.Bounds)) || (h2.Bounds.IntersectsWith(supreme.Bounds)) || h3.Bounds.IntersectsWith(supreme.Bounds))
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                label1.Visible = true;
+            }
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if(voice>25)
+            {
+                supreme.Top += -3;
+
+            }
+            else
+            {
+                if (supreme.Top < 465)
+                    supreme.Top += 2;
+                 
+            }
+            movehurdle();
+            gameOver();
+
+        }
+    }
+}
